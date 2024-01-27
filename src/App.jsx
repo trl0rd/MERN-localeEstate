3bf7d32d78fd1d70afe61e54ab7e702f588c639f
trl0rd <varun110301@gmail.com>
2024-01-28 02:21:52 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-import React from "react";
-
-export default function App() {
-  return <div className="text-red-500">App</div>;
+import React, { Profiler } from "react";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./pages/Profile";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
+
+export default App;
